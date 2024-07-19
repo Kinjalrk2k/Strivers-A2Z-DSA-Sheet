a93174d4e680e7e79d23a2f2b6a4d7cb4d31038f
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2024-07-19 19:38:43 +0530

    feat: skeleton cpp program

---
+++ README.md
@@ -0,0 +1,3 @@
+# Strivers A2Z DSA Sheet
+
+Learning DSA through Striver's A2Z DSA Sheet [here](https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2)
---
+++ Step-01/Lec-03/skeleton.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int a;
+
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << a;
+
+    return 0;
+}
